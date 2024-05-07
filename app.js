3c3f9162c324a4c5bb89b25d60446f4c18dac8fe
yagyaadhikari <yagyaadhikari02gmail.com>
2024-05-07 11:04:21 +0545

    ok

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("YAGYA");
+
+let x = "10";
+let c = 10;
+
+console.log(c == x);
+// var x = 11;
+
+function valX() {
+  var x = 11;
+  console.log(x);
+}
+
+// console.log(x);
+valX();
+
+const apl = ["app", "pple", "ap"];
+
+var dd;
+
+function FOO() {
+  dd = 20;
+  for (i = 0; i < 4; i++) {
+    console.log(dd);
+  }
+  console.log("END")
+}
+FOO();
