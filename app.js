f35fa95d153a7b35cb9b96863a2d8a005af24fa5
yagyaadhikari <yagyaadhikari02gmail.com>
2024-05-10 21:28:22 +0545

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("YAGYA");
+/* console.log("YAGYA");
 
 let x = "10";
 let c = 10;
@@ -25,4 +25,33 @@ function FOO() {
   }
   console.log("END")
 }
-FOO();
+FOO(); */
+
+/* STRING */
+
+let aa = "ELEPHANT is wild animal";
+var alp;
+alp = "ANT";
+
+// split:
+const aoo = aa.split(" "); // split: converts sting to array
+// const aoo = aa.split("is"); // split: converts sting to array
+// aoo.map((a) => console.log(a));
+
+// // forEach:
+// aoo.forEach(funC);
+// function funC(item){
+//   console.log(item);
+// }
+
+// console.log(aa, alp, aoo);
+/* 
+const fruits = ["apple", "ball", "cat", "dog"];
+console.log(fruits.includes("ball", 2));
+ */
+let text = "W3Schools";
+let result = Array.isArray(text);
+// console.log(result);
+const fruits = ["apple", "ball", "cat", "dog"];
+const cc = fruits.slice(0,  1);
+console.log(cc);
